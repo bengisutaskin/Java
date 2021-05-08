2394fb5f9f3e2b1899edea3b32c6c02249a01daf
bengisu taşkın <taskinbengisu0@gmail.com>
2021-05-08 16:24:38 +0300

    Java Kampı

---
+++ sayiBulma1/src/sayiBulma1/Main.java
@@ -0,0 +1,26 @@
+package sayiBulma1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar= new int[] {1,2,5,8,7,9,0};
+		int aranacak = 6;
+		boolean varMi= false;
+		
+		for(int sayi :sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+				
+			}
+		}
+		if(varMi) {
+			System.out.println("Sayý mevcuttur");
+		}
+		else {
+			System.out.println("Sayý mevcut deðildir");
+		}
+
+	}
+
+}
