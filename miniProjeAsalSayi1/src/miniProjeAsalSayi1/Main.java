037e7d740299b90953a65696c81245827aed7283
bengisu taşkın <taskinbengisu0@gmail.com>
2021-05-08 16:27:07 +0300

    Java Kampı

---
+++ miniProjeAsalSayi1/src/miniProjeAsalSayi1/Main.java
@@ -0,0 +1,40 @@
+package miniProjeAsalSayi1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number=2;
+		int remainder=number % 2;
+		System.out.println(remainder);
+		boolean isPrime=true;
+		
+		if(number==1) {
+			System.out.println("Sayý Asal Deðildir");
+			return;
+		}
+		
+		if(number<1) {
+			System.out.println("Geçersiz Sayý");
+			return;
+		
+		}
+		
+		for(int i=2;i<number;i++) {
+			if(number % i == 0) {
+				isPrime=false;
+			
+			}
+			
+		}
+		if(isPrime) {
+			System.out.println("Sayý Asaldýr");
+			
+		}
+		else {
+			System.out.println("Sayý Asal Deðildir");
+		}
+		
+
+	}
+
+}
